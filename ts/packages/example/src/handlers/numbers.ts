16f6c2e3b67e4123d2c3b4d1d511eecc1622e0b9
Julian Jelfs <julian.jelfs@gmail.com>
2025-02-07 16:46:07 +0000

    fix command

--- ts/packages/example/src/handlers/numbers.ts
+++ ts/packages/example/src/handlers/numbers.ts
@@ -6,19 +6,15 @@ import { success } from "./success";
 export default async function (req: WithBotClient, res: Response) {
   const client = req.botClient;
   const intOne = client.integerArg("int_one");
-  const intTwo = client.integerArg("int_two");
   const decOne = client.decimalArg("dec_one");
-  const decTwo = client.decimalArg("dec_two");
 
   if (
     intOne !== undefined &&
-    intTwo !== undefined &&
     decOne !== undefined &&
-    decTwo !== undefined
   ) {
     const msg = await client.createTextMessage(
-      `${intOne} * ${intTwo} * ${decOne} * ${decTwo} = ${
-        Number(intOne) * Number(intTwo) * decOne * decTwo
+      `${intOne} * ${decOne} = ${
+        Number(intOne) * decOne  
       }`
     );
     msg.setFinalised(true);
