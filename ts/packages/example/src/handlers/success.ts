c45629a74ad48ac3a3c1fafa9a1589990c4ff198
Julian Jelfs <julian.jelfs@gmail.com>
2025-02-08 14:34:44 +0000

    fix blm

--- ts/packages/example/src/handlers/success.ts
+++ ts/packages/example/src/handlers/success.ts
@@ -1,5 +1,13 @@
 import { Message } from "@open-ic/openchat-botclient-ts";
 
+//@ts-ignore
+// Uint8Array.prototype.toJSON = function () {
+//   return {
+//     type: "Uint8Array",
+//     bytes: Array.from(this),
+//   };
+// };
+
 export function success(msg?: Message) {
   return {
     message: msg?.toResponse(),
