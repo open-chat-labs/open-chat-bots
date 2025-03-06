2daacd3ab14131236a09796f77c6dc613470fafd
megrogan <megrogan@gmail.com>
2025-03-06 19:20:20 +0000

    Add some install scripts

--- rs/scripts/install_reminder_bot.sh
+++ rs/scripts/install_reminder_bot.sh
@@ -1,10 +1,29 @@
+#!/bin/bash
+
 # Pre-requisites
 #
-# This script assumes the [OpenChat repo](https://github.com/open-chat-labs/open-chat) and the [bot SDK repo](https://github.com/open-chat-labs/open-chat-bots) have been cloned to the same parent folder.
-# And that OpenChat has been setup according to [these instructions](https://github.com/open-chat-labs/open-chat/blob/master/README.md) and dfx is running.
+# 1. The [OpenChat repo](https://github.com/open-chat-labs/open-chat) and the [bot SDK repo](https://github.com/open-chat-labs/open-chat-bots) should be cloned to the same parent folder.
+# 2. OpenChat should be setup according to [these instructions](https://github.com/open-chat-labs/open-chat/blob/master/README.md) 
+# 3. dfx has been started
+# 4. You are using the desired DFX principal. See `dfx identity use --help` for more information.
 
-dfx canister create reminder_bot --no-wallet
+# Capture the directory this script is installed in and backup to rs
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+cd $SCRIPT_DIR/..
 
-CANISTER_ID=$(dfx canister --network $NETWORK id user_index)
+# Create a new canister for the reminder_bot and build its WASM
+CANISTER_ID=$(./scripts/utils/create_and_build_bot.sh reminder_bot) || exit 1
 
-dfx build --check reminder_bot
+# Extract the OpenChat public key from the user_index
+OC_PUBLIC_KEY=$(./scripts/utils/extract_oc_public_key.sh) || exit 1
+
+# Install the reminder_bot canister
+dfx canister install --quiet --mode install reminder_bot --argument "(variant { Init = record { oc_public_key = \"$OC_PUBLIC_KEY\" } } )" || exit 1
+
+# Return the URL of the reminder_bot
+echo ""
+echo "The reminder_bot has been installed and has the following endpoint:"
+echo ""
+echo "http://$CANISTER_ID.raw.localhost:8080"
+echo ""
