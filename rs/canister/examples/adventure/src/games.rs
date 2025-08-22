f8c07dc5563cb106056ecf14b48407952e793571
megrogan <megrogan@gmail.com>
2025-08-22 16:31:03 +0100

    Try this

--- rs/canister/examples/adventure/src/games.rs
+++ rs/canister/examples/adventure/src/games.rs
@@ -1,17 +1,17 @@
 use lazy_static::lazy_static;
 use std::collections::BTreeMap;
 
-static BUCCANEERS_CACHE_ZCODE: &[u8] = include_bytes!("games/buccaneers_cache.z3");
-static CATSEYE_ZCODE: &[u8] = include_bytes!("games/catseye.z3");
-static COLOSSAL_CAVE_ZCODE: &[u8] = include_bytes!("games/colossal_cave.z3");
-static DEJAVU_ZCODE: &[u8] = include_bytes!("games/dejavu.z3");
-static DUCKME_ZCODE: &[u8] = include_bytes!("games/duckme.z3");
-static FANTASYDIMENSION_ZCODE: &[u8] = include_bytes!("games/fantasydimension.z3");
-static GUSSDEATH_ZCODE: &[u8] = include_bytes!("games/gussdeath.z3");
-static LIBRARY_OF_HORROR_ZCODE: &[u8] = include_bytes!("games/library_of_horror.z3");
-static MOONGLOW_ZCODE: &[u8] = include_bytes!("games/moonglow.z3");
-static SUBMARINE_SABOTAGE_ZCODE: &[u8] = include_bytes!("games/submarine-sabotage.z3");
-static ZORK_ZCODE: &[u8] = include_bytes!("games/zork1.z3");
+static BUCCANEERS_CACHE_ZCODE: &[u8] = include_bytes!("./games/buccaneers_cache.z3");
+static CATSEYE_ZCODE: &[u8] = include_bytes!("./games/catseye.z3");
+static COLOSSAL_CAVE_ZCODE: &[u8] = include_bytes!("./games/colossal_cave.z3");
+static DEJAVU_ZCODE: &[u8] = include_bytes!("./games/dejavu.z3");
+static DUCKME_ZCODE: &[u8] = include_bytes!("./games/duckme.z3");
+static FANTASYDIMENSION_ZCODE: &[u8] = include_bytes!("./games/fantasydimension.z3");
+static GUSSDEATH_ZCODE: &[u8] = include_bytes!("./games/gussdeath.z3");
+static LIBRARY_OF_HORROR_ZCODE: &[u8] = include_bytes!("./games/library_of_horror.z3");
+static MOONGLOW_ZCODE: &[u8] = include_bytes!("./games/moonglow.z3");
+static SUBMARINE_SABOTAGE_ZCODE: &[u8] = include_bytes!("./games/submarine-sabotage.z3");
+static ZORK_ZCODE: &[u8] = include_bytes!("./games/zork1.z3");
 
 lazy_static! {
     pub static ref GAMES: BTreeMap<&'static str, &'static [u8]> = BTreeMap::from([
