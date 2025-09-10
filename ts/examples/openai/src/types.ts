2688b132df2d4734a6c94ca7fa764d4ff1c72e74
Julian Jelfs <julian.jelfs@gmail.com>
2025-09-10 11:52:01 +0100

    undo some experiments

--- ts/examples/openai/src/types.ts
+++ ts/examples/openai/src/types.ts
@@ -1,19 +1,8 @@
-import {
-    BotClient,
-    BotEventWrapper,
-    ImageContent,
-    MessageEvent,
-    TextContent,
-} from "@open-ic/openchat-botclient-ts";
+import { BotClient, ImageContent, MessageEvent, TextContent } from "@open-ic/openchat-botclient-ts";
 import { Request } from "express";
 
 export interface WithBotClient extends Request {
     botClient: BotClient;
-}
-
-export interface WithBotEvent extends WithBotClient {
-    botClient: BotClient;
-    botEventWrapper: BotEventWrapper;
 }
 
 export type ModeratableContent = MessageEvent<TextContent> | MessageEvent<ImageContent>;
